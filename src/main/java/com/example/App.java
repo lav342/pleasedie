f5b271ef52110f0f098d05778ef1217042945d0e
lav342 <dalavanya284@gmail.com>
2025-12-23 18:43:23 +0530

    Add greeting message to main method

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,5 +3,6 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Jenkins Maven Build");
+        System.out.println("hi");
     }
 }
